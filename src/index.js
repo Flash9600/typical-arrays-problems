085617a92f69699f9054807bda8149d822dbb5a3
Flash9600 <trefilovdimarus@gmail.com>
2020-10-02 18:53:42 +0400

    init:

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array == undefined || array.length == 0){
+    return 0;
+  }
+  return Math.min( ...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined || array.length == 0){
+    return 0;
+  }
+  return Math.max( ...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined || array.length == 0){
+    return 0;
+  }
+  let result=0;
+  for(let i=0; i<array.length; i++){
+     result += array[i];
+  }
+  if (result ===0) {return 0}
+  return result/array.length;
 }
